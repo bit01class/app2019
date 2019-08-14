7a330dfd2609e1feb7e3dfa1b2c39ec453c637e4
bit03class <bit01class@gmail.com>
2019-08-14 12:00:11 +0900

    security init - inMemoryAuthentication()

---
+++ sts01/src/main/java/com/bit/config/SecurityIntiallizer.java
@@ -0,0 +1,7 @@
+package com.bit.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityIntiallizer extends AbstractSecurityWebApplicationInitializer {
+
+}
